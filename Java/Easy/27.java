27a568764cffb852a5d0e186dbfc42a90a6ee031
Xstroke1820 <dhruv.jasathy@gmail.com>
2024-10-05 13:02:15 +0530

    Update 27.java
    java--easy--q27

--- Java/Easy/27.java
+++ Java/Easy/27.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
+        int numbers[] = new int[5];
         for (int i = 0; i <= 5; i++) {
             numbers[i] = i;
         }
