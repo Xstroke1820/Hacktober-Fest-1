93230743f8474c2a1b7786c5d2ad8cc1db9a1008
Xstroke1820 <dhruv.jasathy@gmail.com>
2024-10-05 12:39:55 +0530

    Update 16.java
    java--easy--q16

--- Java/Easy/16.java
+++ Java/Easy/16.java
@@ -2,7 +2,9 @@ class Main {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        System.out.println(a > b ? "Greater" : "Smaller");
-        else System.out.println("Equal");
+
+        System.out.println(a>b?"Greater":"Smaller");
+        if(a==b)
+         System.out.println("Equal");
     }
 }
