2cbc71e53872ced3be9cd64f71ef36a893ffc84c
yashrajrohit <yashrajrohit1234@gmail.com>
2024-10-05 13:23:34 +0530

    Update 11.py
    TECHTITANS3 PYQ11

--- Python/Easy/11.py
+++ Python/Easy/11.py
@@ -1,9 +1,11 @@
 def outer():
     x = 10
-    def inner():
-        global x  # Error: x is local to outer and cannot be made global
-        x = 5
-    inner()
-    print(x)
+
+def inner():
+     x # Error: x is local to outer and cannot be made global
+x = 5
+inner()
+print(x)
 
 outer()
+print(x)
