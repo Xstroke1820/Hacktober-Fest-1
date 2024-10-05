a6a8fc9e43816b97dbdb0c8281e3122a5345b18c
Xstroke1820 <dhruv.jasathy@gmail.com>
2024-10-05 13:27:37 +0530

    Update 37.java
    java easy q37

--- Java/Easy/37.java
+++ Java/Easy/37.java
@@ -2,6 +2,7 @@ class Main {
     public static void main(String[] args) {
         int a = 5;
         a++;
-        System.out.println(++a++);
+        ++a;
+        System.out.println(a++);
     }
 }
